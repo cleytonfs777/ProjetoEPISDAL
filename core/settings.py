8f6e097ebc2177de5a414b2d055bb65df48fdfa6
cleytonfs777 <cleytonfs777@gmail.com>
2024-08-21 18:38:28 -0300

    Corrige ALLOWED_HOSTS removendo o protocolo e barras

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,10 @@ SECRET_KEY = 'django-insecure-fi)&f2$&zokq1vsr6g@kie#*ef+f&kyjkwlb2(lmi9hni%q+z&
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "https://projetoepisdal-production.up.railway.app/", "localhost", "127.0.0.1"]
+    "projetoepisdal-production.up.railway.app",
+    "localhost",
+    "127.0.0.1"
+]
 
 AUTH_USER_MODEL = 'usuarios.Users'
 
