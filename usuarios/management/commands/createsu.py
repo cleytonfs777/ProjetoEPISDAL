f68eced9d25b49ef12ee196d2d07657b7c34204a
cleytonfs777 <cleytonfs777@gmail.com>
2024-08-21 20:18:35 -0300

    superu

--- usuarios/management/commands/createsu.py
+++ usuarios/management/commands/createsu.py
@@ -1,9 +1,8 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+User = get_user_model()
 
 
 class Command(BaseCommand):
